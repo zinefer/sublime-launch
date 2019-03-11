8a0c34be309929b7d7c84d3237888b03e22d28f6
zinefer <zinefer@gmail.com>
2019-03-11 11:56:30 -0600

    Move variable population to own function

--- launch.py
+++ launch.py
@@ -3,28 +3,9 @@ import re
 
 class LaunchCommand(sublime_plugin.WindowCommand):
     def run(self, command, parameters=[], cwd=None, variables={}):
-        view = self.window.active_view()
-
-        # Populate variables
+        # Populate variables if we don't have any
         if not variables:
-            # File variables
-            file = view.file_name()
-            if file:
-                variables['file'] = file
-                variables['file_path'] = os.path.split(file)[0]
-                variables['file_name'] = os.path.basename(file)
-
-            # Project variables
-            project = sublime.active_window().project_data()
-            if project and ('folders' in project):
-                variables['project_folder'] = project['folders'][0]['path']
-                for folder in range(len(project['folders'])):
-                    variables['project_folder[' + str(folder) + ']'] = project['folders'][folder]['path']
-
-            # Selected text variable
-            if (view.sel())[0].size() > 0:
-                # Combine all selections if there are more than one
-                variables['selected_text'] = functools.reduce((lambda a, b: a + view.substr(b)), view.sel(), '')
+            variables = self.populate_variables()
 
 
         # Create a callback to be used if a replacement needs user input
@@ -50,30 +31,68 @@ class LaunchCommand(sublime_plugin.WindowCommand):
         self.launch_it(command, parameters, cwd)
         variables.clear()
 
+    # Inspired by sublime-text-shell-command
+    # Replaces variable tokens with their values or raises an exception for user input
     def expand_variables(self, string, variables):
-        ''' Inspired by sublime-text-shell-command
-        ${<variable_name>[:[default value][:<Prompt message if not exist>]]}
-        EX) git branch -m ${current_branch} ${new_branch::Enter branch name}
-        '''
         matches = re.findall(r'\${(.*?)}', string)
         for variable_def in matches:
             raw = '${' + variable_def + '}'
             name, default, prompt = self.parse_variable(variable_def)
 
-            if not variables.get(name):
+            if not name in variables:
                 if prompt: raise RequireUserInputError(name, default, prompt)
                 if default: variables[name] = default
 
             string = string.replace(raw, variables[name])
         return string
 
+    def populate_variables(self):
+        view = self.window.active_view()
+        variables = {}
+
+        # Project variables
+        project = sublime.active_window().project_data()
+        if project:
+            if 'folders' in project:
+                # Add project folders
+                variables['project_folder'] = project['folders'][0]['path']
+                for folder in range(len(project['folders'])):
+                    variables["project_folder[{0:d}]".format(folder)] = project['folders'][folder]['path']
+            if 'launch_variables' in project:
+                # Merge project variables
+                variables.update(project['launch_variables'])
+
+        project_file = sublime.active_window().project_file_name()
+        if project_file:
+            variables['project'] = os.path.basename(os.path.splitext(project_file)[0])
+
+        # File variables
+        file = view.file_name()
+        if file:
+            variables['file'] = file
+            variables['file_path'] = os.path.split(file)[0]
+            variables['file_name'] = os.path.basename(file)
+            if project and ('folders' in project):
+                variables['file_poject_path'] = os.path.relpath(file, project['folders'][0]['path'])
+
+        # Selected text variable
+        if (view.sel())[0].size() > 0:
+            # Combine all selections if there are more than one
+            variables['selected_text'] = functools.reduce((lambda a, b: a + view.substr(b)), view.sel(), '')
+
+        return variables
+
+    # Format: ${name:default:prompt}
     def parse_variable(self, variable):
         parts = variable.split(':')
         return [parts[i] if i < len(parts) else None for i in range(3)]
 
     def launch_it(self, command, parameters, cwd):
+        if sublime.platform() == "windows" and ' ' in command:
+            command = '"' + command + '"'
+
         compiled_command = ' '.join([command] + parameters)
-        print('Launching `' + compiled_command + '`' + (' in ' + cwd) if cwd else '')
+        print('Launching `' + compiled_command + '`', ('in ' + cwd if cwd else ''))
         try:
             p = subprocess.Popen(compiled_command, cwd=cwd)
         except Exception as e:
